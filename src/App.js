ba2b29986dca68bebf5ce0fca1a5b97f20882e36
arjunn2313 <arjunnks123@gmail.com>
2024-01-10 19:07:51 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+ 
+function App() {
+  return (
+    <div className="text-white grid grid-cols-9">
+     <div className="col-span-2">hi</div>
+     <div className="col-span-5">hi</div>
+     <div className="col-span-2">hi</div>
+    </div>
+  );
+}
+
+export default App;
